b5d66e31f40ef1d8927ed82d552718ead72f40c6
6lackjid <6lackjid>
2022-08-22 21:07:04 +0900

    データ作成

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const morgan = require("morgan");
+const controller = require('./controller')
+app.use(express.json());
+
+app.use(morgan("common"));
+
+app.use('/memo', controller.memo())
+
+app.listen(port, () => {
+  console.log(`example app listening at http://localhost:${port}`);
+});
---
+++ usecase/memo.js
@@ -0,0 +1,8 @@
+const service = require('../service')
+
+exports.getAll = () => {
+  const memos = service.memo.getAll()
+
+
+  return memos
+};
